28e7c0ed40c8a021a64113446b01e3b84ef9eaf3
Shvydenko Dmytro <cuderlis.deus@gmail.com>
2025-08-08 15:07:58 +0300

    added

--- config/bspwm/rices/deus_pink/test.sh
+++ config/bspwm/rices/deus_pink/test.sh
@@ -1,5 +1,5 @@
-cd ~/bspwm-dotfiles/config/bspwm/rices/deus
+cd ~/bspwm-dotfiles/config/bspwm/rices/deus_pink
 git pull
-rm  ~/.config/bspwm/rices/deus/config.ini
-rm  ~/.config/bspwm/rices/deus/modules.ini
-cp *.ini ~/.config/bspwm/rices/deus
+rm  ~/.config/bspwm/rices/deus_pink/config.ini
+rm  ~/.config/bspwm/rices/deus_pink/modules.ini
+cp *.ini ~/.config/bspwm/rices/deus_pink
