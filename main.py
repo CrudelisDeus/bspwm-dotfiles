86a28a2c979d483ebf87be55facbe1a61e9c3fda
Dmytro <karakurt.deus@gmail.com>
2026-04-04 16:47:00 +0300

    init dbus

---
+++ main.py
@@ -0,0 +1,54 @@
+import subprocess 
+
+# pkg 
+std_pkg = [
+    # python
+    "python",
+    
+    # fonts
+    "ttf-dejavu",
+    "ttf-font-awesome",
+    "ttf-nerd-fonts-symbols",
+    "ttf-jetbrains-mono-nerd",
+    "noto-fonts",
+
+    # dbus
+    "dbus",
+
+    # xorg
+    "xorg-xinit",
+    "xorg",
+
+    # bspwm
+    "bspwm",
+    "sxhkd",
+
+
+]
+
+# function
+
+def install_pkg(pkgs: list) -> None:
+    for pkg in pkgs:
+        try:
+            subprocess.run(
+                ['sudo', 'pacman', '-S', pkg, '--noconfirm'],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+            result = 0
+        except Exception as e:
+            result = 1
+
+        if result != 0:
+            print(f'pkg FAILED: {pkg}')
+        else:
+            print(f'pkg OK: {pkg}')
+
+# start
+def main():
+    install_pkg(std_pkg)
+
+if __name__ == '__main__':
+    main()
