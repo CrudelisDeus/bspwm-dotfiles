db7701a81b46d601d1287d22fbdf55616772a411
Dmytro <karakurt.deus@gmail.com>
2026-04-07 20:31:27 +0300

    added clipt history manager

--- main.py
+++ main.py
@@ -91,6 +91,15 @@ std_pkg = [
     "flameshot",
 ]
 
+yay_pkg = [
+    'base-devel',
+    'git',
+]
+
+yay_list_pkg = [
+    'greenclip'
+]
+
 work_and_home_pkg = [
     "telegram-desktop",
     "discord",
@@ -115,10 +124,77 @@ def install_pkg(pkgs: list) -> None:
         else:
             print(f'pkg OK: {pkg}')
 
+# yay install
+def clone_yay():
+    try:
+        subprocess.run(
+            [
+                'git',
+                'clone',
+                'https://aur.archlinux.org/yay.git',
+                '/tmp/yay'
+            ],
+            check=True,
+            capture_output=True,
+            text=True
+        )
+        result = 0
+    except Exception:
+        result = 1
 
+    if result != 0:
+        print(f'yay FAILED: clone')
+    else:
+        print(f'yay OK: clone')
+
+def configure_yay():
+    try:
+        subprocess.run(
+            ['makepkg', '-si', '--noconfirm'],
+            check=True,
+            text=True,
+            cwd='/tmp/yay',
+            capture_output=True,
+        )
+        result = 0
+    except Exception:
+        result = 1
+
+    if result != 0:
+        print(f'yay FAILED: configure')
+    else:
+        print(f'yay OK: configure')
+
+def install_yay_pkg(pkgs: list) -> None:
+    for pkg in pkgs:
+        try:
+            subprocess.run(
+                ['yay', '-S', pkg, '--noconfirm', '--needed'],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+            result = 0
+        except Exception:
+            result = 1
+
+        if result != 0:
+            print(f'yay pkg FAILED: {pkg}')
+        else:
+            print(f'yay pkg OK: {pkg}')
+
+def install_yay():
+    install_pkg(yay_pkg)
+    clone_yay()
+    configure_yay()
+
+# start programm
 def main():
     install_pkg(std_pkg)
     install_pkg(work_and_home_pkg)
+
+    install_yay()
+    install_yay_pkg(yay_list_pkg)
 
 
 if __name__ == '__main__':
