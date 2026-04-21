f82f3173cb97dbfc205ec7105a767a934a44a8d2
Dmytro <karakurt.deus@gmail.com>
2026-04-21 18:16:42 +0300

    fix nvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 local map = vim.keymap.set
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 -- Save current file
 map("n", "<leader>w", ":w<cr>", { desc = "Save file", remap = true })
